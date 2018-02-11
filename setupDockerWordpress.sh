904c4c3a655bc972aedf316013e0ea8556726dc1
anthrax3 <anthrax3@hotmail.com>
2018-02-11 07:28:15 +0200

    Create setupDockerWordpress.sh

---
+++ setupDockerWordpress.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# setup docker 
+# get mysql & wordpress images
+# test & run with firefox
+# for lazy developers :)
+# author Bosz Mand
+# facebook: https://www.facebook.com/yalim.kankaya
+
+
+echo Sitename?
+read sitename
+
+echo "sitename : " $sitename
+
+
+echo Password?
+read password
+
+echo "nice password : " $password
+
+echo Port?
+read port
+
+echo "port : " $port
+
+echo "mysql Name ?"
+read mysqlname
+
+echo "mysql name : " $mysqlname
+
+sudo apt-get install docker.io -y
+wait & sudo docker pull mysql
+wait & sudo docker pull wordpress 
+wait & sudo docker run -d --name $mysqlname -e MYSQL_ROOT_PASSWORD=$password mysql
+wait & sudo docker run -d --name $sitename --link $mysqlname:mysql -p $port:80 wordpress
+wait & sleep 5s 
+wait & firefox http://localhost:$port 
